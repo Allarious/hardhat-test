65d4ddbee88f52f370a111a5e5eb45c80ec3aed9
allarious <arjmands@ualberta.ca>
2022-07-19 21:27:01 -0600

    experimenting with tasks

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,5 +1,6 @@
-import { HardhatUserConfig, task } from "hardhat/config";
+import { HardhatUserConfig, task, types } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
+import "@nomiclabs/hardhat-ethers"
 
 const config: HardhatUserConfig = {
   solidity: { 
@@ -20,5 +21,26 @@ task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
     console.log(account.address)
   }
 })
+
+task("balance", "Prints an account's balance")
+  .addParam("account", "The account's address")
+  .setAction(async (taskArgs, hre) => {
+    const balance = await hre.ethers.provider.getBalance(taskArgs.account);
+
+    console.log(hre.ethers.utils.formatEther(balance), "ETH");
+  });
+
+// task("hello", "Prints a greeting'")
+//   .addOptionalParam("greeting", "The greeting to print", "Hello, World!")
+//   .setAction(async ({ greeting }) => console.log(greeting));
+
+task("hello", "Prints hello mutiple times.")
+  .addOptionalParam("times", "how many times hello is printed", 1, types.int)
+  .setAction(async ({ times }) => {
+    for(let i = 0; i < times; i++){
+      console.log("Hello")
+    }
+  })
+
 
 export default config;
